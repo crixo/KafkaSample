1034d70bf355902affcaf5eb8036cbd995f9b5d7
cristiano-degiorgis <cri@webprofessor.it>
2018-04-07 20:16:17 +0200

    docker

--- BasicProducer/Program.cs
+++ BasicProducer/Program.cs
@@ -2,11 +2,99 @@
 
 namespace BasicProducer
 {
-    class Program
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using Confluent.Kafka;
+    using Confluent.Kafka.Serialization;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //if (args.Length != 2)
+            //{
+            //    Console.WriteLine("Usage: .. brokerList topicName");
+            //    return;
+            //}
+
+            string brokerList = "192.168.1.7:9092";
+            string topicName = "my-topic";
+
+            var config = new Dictionary<string, object> { 
+                { "bootstrap.servers", brokerList },
+                //{ "auto.create.topics.enable", true }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                PrintMetadata(brokerList);
+                ListGroups(brokerList);
+                Console.WriteLine($"{producer.Name} producing on {topicName}. q to exit.");
+
+
+                string text;
+                while ((text = Console.ReadLine()) != "q")
+                {
+                    if (string.IsNullOrEmpty(text)) continue;
+                    var deliveryReport = producer.ProduceAsync(topicName, null, text);
+                    deliveryReport.ContinueWith(task =>
+                    {
+                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                    });
+                }
+
+                // Tasks are not waited on synchronously (ContinueWith is not synchronous),
+                // so it's possible they may still in progress here.
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        static void PrintMetadata(string brokerList)
+        {
+            var config = new Dictionary<string, object> { { "bootstrap.servers", brokerList } };
+            using (var producer = new Producer(config))
+            {
+                var meta = producer.GetMetadata(true, null);
+                Console.WriteLine($"{meta.OriginatingBrokerId} {meta.OriginatingBrokerName}");
+                meta.Brokers.ForEach(broker =>
+                    Console.WriteLine($"Broker: {broker.BrokerId} {broker.Host}:{broker.Port}"));
+
+                meta.Topics.ForEach(topic =>
+                {
+                    Console.WriteLine($"Topic: {topic.Topic} {topic.Error}");
+                    topic.Partitions.ForEach(partition =>
+                    {
+                        Console.WriteLine($"  Partition: {partition.PartitionId}");
+                        Console.WriteLine($"    Replicas: {string.Join(",", partition.Replicas)}");
+                        Console.WriteLine($"    InSyncReplicas: {string.Join(",", partition.InSyncReplicas)}");
+                    });
+                });
+            }
+        }
+
+        static void ListGroups(string brokerList)
+        {
+            var config = new Dictionary<string, object> { { "bootstrap.servers", brokerList } };
+
+            using (var producer = new Producer(config))
+            {
+                var groups = producer.ListGroups(TimeSpan.FromSeconds(10));
+                Console.WriteLine($"Consumer Groups:");
+                foreach (var g in groups)
+                {
+                    Console.WriteLine($"  Group: {g.Group} {g.Error} {g.State}");
+                    Console.WriteLine($"  Broker: {g.Broker.BrokerId} {g.Broker.Host}:{g.Broker.Port}");
+                    Console.WriteLine($"  Protocol: {g.ProtocolType} {g.Protocol}");
+                    Console.WriteLine($"  Members:");
+                    foreach (var m in g.Members)
+                    {
+                        Console.WriteLine($"    {m.MemberId} {m.ClientId} {m.ClientHost}");
+                        Console.WriteLine($"    Metadata: {m.MemberMetadata.Length} bytes");
+                        Console.WriteLine($"    Assignment: {m.MemberAssignment.Length} bytes");
+                    }
+                }
+            }
         }
     }
 }
--- SimpleConsumer/Program.cs
+++ SimpleConsumer/Program.cs
@@ -1,12 +1,112 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
 
 namespace SimpleConsumer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Consumer_Seek("192.168.1.7:9092", "my-topic", "my-topic");
+            IConfiguration config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+            
+            Test1(config);
+        }
+
+        public static void Test1(IConfiguration config)
+        {
+            var conf = new Dictionary<string, object>
+    {
+      { "group.id", "test-consumer-group-02" },
+      { "bootstrap.servers", "192.168.1.7:9092" },
+      { "auto.commit.interval.ms", 5000 },
+                { "auto.offset.reset", "earliest" }
+    };
+
+            using (var consumer = new Consumer<Null, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                
+
+
+                consumer.OnMessage += (_, msg)
+                  => Console.WriteLine($"Read '{msg.Value}' from: {msg.TopicPartitionOffset}");
+
+                consumer.OnError += (_, error)
+                  => Console.WriteLine($"Error: {error}");
+
+                consumer.OnConsumeError += (_, msg)
+                  => Console.WriteLine($"Consume error ({msg.TopicPartitionOffset}): {msg.Error}");
+
+                if(config["OFFSET"]!=null)
+                {
+                    Console.WriteLine(config["OFFSET"]);
+
+                    long offsetFromConfig;
+                    var offset = long.TryParse(config["OFFSET"], out offsetFromConfig) ? offsetFromConfig : 0;
+                    consumer.Assign(new TopicPartitionOffset[] { new TopicPartitionOffset("my-topic", 0, offset) });
+                    consumer.Seek(new TopicPartitionOffset(new TopicPartition("my-topic", 0), new Offset(offset))); 
+                }
+                else
+                {
+                    consumer.Subscribe("my-topic"); 
+                }
+
+
+                while (true)
+                {
+                    consumer.Poll(TimeSpan.FromMilliseconds(100));
+                }
+            }
+        }
+
+        public static void Consumer_Seek(string bootstrapServers, string singlePartitionTopic, string partitionedTopic)
+        {
+            var consumerConfig = new Dictionary<string, object>
+            {
+                { "group.id",  "test"},//Guid.NewGuid().ToString()
+                { "acks", "all" },
+                { "bootstrap.servers", bootstrapServers }
+            };
+
+            var producerConfig = new Dictionary<string, object> { { "bootstrap.servers", bootstrapServers } };
+
+            using (var producer = new Producer<Null, string>(producerConfig, null, new StringSerializer(Encoding.UTF8)))
+            using (var consumer = new Consumer<Null, string>(consumerConfig, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnError += (_, e)
+                    => Console.WriteLine($"Error: {e}");
+
+                consumer.OnConsumeError += (_, e) =>
+                    Console.WriteLine($"Error: {e}");
+
+                const string checkValue = "check value";
+                var dr = producer.ProduceAsync(singlePartitionTopic, null, checkValue).Result;
+                var dr2 = producer.ProduceAsync(singlePartitionTopic, null, "second value").Result;
+                var dr3 = producer.ProduceAsync(singlePartitionTopic, null, "third value").Result;
+
+                Console.WriteLine(dr.Offset);
+
+                consumer.Assign(new TopicPartitionOffset[] { new TopicPartitionOffset(singlePartitionTopic, 0, dr.Offset) });
+
+                Message<Null, string> message;
+                Console.WriteLine(consumer.Consume(out message, TimeSpan.FromSeconds(30)));
+                Console.WriteLine(consumer.Consume(out message, TimeSpan.FromSeconds(30)));
+                Console.WriteLine(consumer.Consume(out message, TimeSpan.FromSeconds(30)));
+                consumer.Seek(dr.TopicPartitionOffset);
+                Console.WriteLine($"Topic: {dr.TopicPartitionOffset.Topic} - Partiotion: {dr.TopicPartitionOffset.Partition} - Offset: {dr.TopicPartitionOffset.Offset}");
+
+                Console.WriteLine(consumer.Consume(out message, TimeSpan.FromSeconds(30)));
+                Console.WriteLine(checkValue + "---" + message.Value);
+            }
         }
     }
 }
+
