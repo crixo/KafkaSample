a18b6aec227bbad79902c991fe3d4b2b73b1b305
cristiano-degiorgis <cri@webprofessor.it>
2018-04-07 20:14:38 +0200

    first commit

---
+++ BasicProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
